5c7f580a6f1357d18d9a0af178b5b84c6ce89d80
Pabloo22 <pablete.arino@gmail.com>
2023-12-15 22:38:40 +0100

    Add downsample_block, upsample_block, middle_block functions and build_generator, build_discriminator functions

--- water_mark_remover/building_blocks.py
+++ water_mark_remover/building_blocks.py
@@ -1,4 +1,3 @@
-
 import keras
 from keras.models import Model
 from tensorflow import keras
@@ -10,144 +9,161 @@ from keras.layers import (
     Conv2D,
     UpSampling2D,
     BatchNormalization,
+    concatenate,
+    Flatten,
+    Dense,
+    LeakyReLU,
+    Dropout,
+    Activation,
 )
 
 
-def downsample_block(x, filters, kernel_size=(3, 3), padding="same", activation="relu"):
-    # downsample
-    conv1 = Conv2D(
+def downsample_block(x, filters, kernel_size=(3, 3), padding="same", strides=(1, 1)):
+    """Create a downsample block."""
+    conv = Conv2D(
+        filters,
+        kernel_size,
+        strides=strides,
+        padding=padding,
+        kernel_initializer="he_normal",
+    )(x)
+    conv = BatchNormalization()(conv)
+    conv = Conv2D(
+        filters,
+        kernel_size,
+        activation="relu",
+        padding=padding,
+        kernel_initializer="he_normal",
+    )(conv)
+    conv = BatchNormalization()(conv)
+    pool = MaxPooling2D(pool_size=(2, 2))(conv)
+    return conv, pool
+
+
+def upsample_block(x, skip_connection, filters, kernel_size=(3, 3), padding="same"):
+    """Create an upsample block."""
+    up = Conv2D(
+        filters,
+        kernel_size=(2, 2),
+        activation="relu",
+        padding="same",
+        kernel_initializer="he_normal",
+    )(UpSampling2D(size=(2, 2))(x))
+    up = BatchNormalization()(up)
+    merge = concatenate([skip_connection, up], axis=3)
+    conv = Conv2D(
+        filters,
+        kernel_size,
+        activation="relu",
+        padding=padding,
+        kernel_initializer="he_normal",
+    )(merge)
+    conv = BatchNormalization()(conv)
+    conv = Conv2D(
+        filters,
+        kernel_size,
+        activation="relu",
+        padding=padding,
+        kernel_initializer="he_normal",
+    )(conv)
+    conv = BatchNormalization()(conv)
+    return conv
+
+
+def middle_block(x, filters, kernel_size=(3, 3), padding="same"):
+    """Create the middle block of the network."""
+    conv = Conv2D(
         filters,
         kernel_size,
         activation="relu",
         padding=padding,
         kernel_initializer="he_normal",
     )(x)
-    conv1 = BatchNormalization()(conv1)
-    conv1 = Conv2D(
-        64, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(conv1)
-    conv1 = BatchNormalization()(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)  # 98
+    conv = BatchNormalization()(conv)
+    conv = Conv2D(
+        filters,
+        kernel_size,
+        activation="relu",
+        padding=padding,
+        kernel_initializer="he_normal",
+    )(conv)
+    conv = BatchNormalization()(conv)
+    return conv
 
-    return pool1
+
+def build_generator(width, height):
+    """Build the generator model."""
+    inputs = Input(shape=(width, height, 3))
+
+    # Downsample
+    d1, p1 = downsample_block(inputs, 64)
+    d2, p2 = downsample_block(p1, 128)
+    d3, p3 = downsample_block(p2, 256)
+    d4, p4 = downsample_block(p3, 512)
+
+    # Middle
+    middle = middle_block(p4, 1024)
+
+    # Upsample
+    u1 = upsample_block(middle, d4, 512)
+    u2 = upsample_block(u1, d3, 256)
+    u3 = upsample_block(u2, d2, 128)
+    u4 = upsample_block(u3, d1, 64)
+
+    # Output
+    output = Conv2D(3, (1, 1), activation="sigmoid")(u4)
+
+    return Model(inputs, output)
 
 
-def generate_generator(width, height):
-    x = Input(shape=(width, height, 3))  # 196
+def build_discriminator(
+    input_shape,
+    filters,
+    kernel_size,
+    strides,
+    activation="leaky_relu",
+    alpha=0.2,
+    dropout_rate=0.3,
+    use_batch_norm=True,
+    dense_units=512,
+    final_activation="sigmoid",
+):
+    """Builds the discriminator model."""
 
-    # downsample
-    conv1 = Conv2D(
-        64, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(x)
-    conv1 = BatchNormalization()(conv1)
-    conv1 = Conv2D(
-        64, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(conv1)
-    conv1 = BatchNormalization()(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)  # 98
+    if (
+        len(filters) != len(strides)
+    ):
+        raise ValueError(
+            f"The length of filters ({len(filters)})and strides ({len(strides)}) must be equal."
+        )
 
-    conv2 = Conv2D(
-        128, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(pool1)
-    conv2 = BatchNormalization()(conv2)
-    conv2 = Conv2D(
-        128, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(conv2)
-    conv2 = BatchNormalization()(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    # Input Layer
+    inputs = Input(shape=input_shape)
 
-    conv3 = Conv2D(
-        256, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(pool2)
-    conv3 = BatchNormalization()(conv3)
-    conv3 = Conv2D(
-        256, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(conv3)
-    conv3 = BatchNormalization()(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    x = inputs
 
-    conv4 = Conv2D(
-        512, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(pool3)
-    conv4 = BatchNormalization()(conv4)
-    conv4 = Conv2D(
-        512, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(conv4)
-    conv4 = BatchNormalization()(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    # Convolutional layers
+    for n_filters, stride in zip(filters, strides):
+        x = Conv2D(
+            n_filters,
+            kernel_size,
+            strides=stride,
+            padding="same",
+            kernel_initializer="he_normal",
+        )(x)
+        if activation == "leaky_relu":
+            x = LeakyReLU(alpha=alpha)(x)
+        else:
+            x = Activation(activation)(x)
+        if use_batch_norm:
+            x = BatchNormalization()(x)
+        x = Dropout(dropout_rate)(x)
 
-    # middle
-    conv5 = Conv2D(
-        1024, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(pool4)
-    conv5 = BatchNormalization()(conv5)
-    conv5 = Conv2D(
-        1024, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(conv5)
-    conv5 = BatchNormalization()(conv5)
+    # Flatten and Dense layers
+    x = Flatten()(x)
+    x = Dense(dense_units, activation="relu")(x)
 
-    # upsample
-    up6 = Conv2D(
-        512, 2, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(UpSampling2D(size=(2, 2))(conv5))
-    up6 = BatchNormalization()(up6)
-    merge6 = keras.layers.concatenate([conv4, up6], axis=3)
-    conv6 = Conv2D(
-        512, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(merge6)
-    conv6 = BatchNormalization()(conv6)
-    conv6 = Conv2D(
-        512, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(conv6)
-    conv6 = BatchNormalization()(conv6)
+    # Output layer
+    output = Dense(1, activation=final_activation)(x)
 
-    up7 = Conv2D(
-        256, 2, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(UpSampling2D(size=(2, 2))(conv6))
-    up7 = BatchNormalization()(up7)
-    merge7 = keras.layers.concatenate([conv3, up7], axis=3)
-    conv7 = Conv2D(
-        256, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(merge7)
-    conv7 = BatchNormalization()(conv7)
-    conv7 = Conv2D(
-        256, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(conv7)
-    conv7 = BatchNormalization()(conv7)
-
-    up8 = Conv2D(
-        128, 2, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(UpSampling2D(size=(2, 2))(conv7))
-    up8 = BatchNormalization()(up8)
-    merge8 = keras.layers.concatenate([conv2, up8], axis=3)
-    conv8 = Conv2D(
-        128, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(merge8)
-    conv8 = BatchNormalization()(conv8)
-    conv8 = Conv2D(
-        128, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(conv8)
-    conv8 = BatchNormalization()(conv8)
-
-    up9 = Conv2D(
-        64, 2, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(UpSampling2D(size=(2, 2))(conv8))
-    up9 = BatchNormalization()(up9)
-    merge9 = keras.layers.concatenate([conv1, up9], axis=3)
-    conv9 = Conv2D(
-        64, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(merge9)
-    conv9 = BatchNormalization()(conv9)
-    conv9 = Conv2D(
-        64, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(conv9)
-    conv9 = BatchNormalization()(conv9)
-    conv9 = Conv2D(
-        2, 3, activation="relu", padding="same", kernel_initializer="he_normal"
-    )(conv9)
-    conv9 = BatchNormalization()(conv9)
-
-    # output
-    output = Conv2D(3, 1, activation="sigmoid")(conv9)
-
-    return Model(inputs=x, outputs=output)
+    return Model(inputs, output)
