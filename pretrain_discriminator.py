57292cc5e4582d9f09974059e710036385297541
Pabloo22 <pablete.arino@gmail.com>
2023-12-16 12:47:20 +0100

    Add discriminator sweep configuration file and update training script

--- pretrain_discriminator.py
+++ pretrain_discriminator.py
@@ -1,11 +1,13 @@
 import yaml
 import numpy as np
+import wandb
 
 from water_mark_remover import load_and_prepare_data, build_discriminator, get_optimizer
 
 
 CONFIG_PATH = "config/"
 CONFIG_FILE_NAME = "pretrained_discriminator.yml"
+SWEEP_CONFIG_FILE_NAME = "discriminator_sweep.yml"
 
 
 def construct_dataset(wm_images: np.ndarray, nowm_images: np.ndarray):
@@ -15,12 +17,28 @@ def construct_dataset(wm_images: np.ndarray, nowm_images: np.ndarray):
     return x, y
 
 
-def train(config: dict):
-    X_train, X_test, y_train, y_test, X_val, y_val = load_and_prepare_data(
+def update_config(config: dict):
+    """Update the configuration with wandb's hyperparameters"""
+    for key in wandb.config.keys():
+        if key in config:
+            config[key] = wandb.config[key]
+        elif any(key in config[subsection] for subsection in config):
+            for subsection in config:
+                if key in config[subsection]:
+                    config[subsection][key] = wandb.config[key]
+
+
+def train():
+    with open(CONFIG_PATH + CONFIG_FILE_NAME, "r") as f:
+        config = yaml.safe_load(f)
+
+    # Update the configuration with wandb's hyperparameters
+    update_config(config)
+
+    X_train, _, y_train, _, X_val, y_val = load_and_prepare_data(
         **config["load_and_prepare_data"]
     )
     X_train, y_train = construct_dataset(X_train, y_train)
-    X_test, y_test = construct_dataset(X_test, y_test)
     X_val, y_val = construct_dataset(X_val, y_val)
 
     discriminator = build_discriminator(**config["build_discriminator"])
@@ -35,15 +53,15 @@ def train(config: dict):
         y_train,
         validation_data=(X_val, y_val),
         **config["fit"],
+        callbacks=[wandb.keras.WandbCallback()]
     )
 
 
 def main():
-    # Read config file
-    with open(CONFIG_PATH + CONFIG_FILE_NAME, "r") as f:
-        config = yaml.safe_load(f)
-
-    train(config)
+    with open(CONFIG_PATH + SWEEP_CONFIG_FILE_NAME, "r") as f:
+        sweep_config = yaml.safe_load(f)
+    sweep_id = wandb.sweep(sweep_config, project="your_project_name")
+    wandb.agent(sweep_id, train)
 
 
 if __name__ == "__main__":
